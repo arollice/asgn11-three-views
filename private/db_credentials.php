a65c9d0db556b900c7f5a567076438463d111f91
ashleerollice@MB-P <ashleerollice@students.abtech.edu>
2024-12-03 15:38:08 -0500

    credential changes

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -7,9 +7,9 @@
 
 // localhost
 define("DB_SERVER", "localhost");
-define("DB_USER", " ");
-define("DB_PASS", " ");
-define("DB_NAME", " ");
+define("DB_USER", "arollice_hbic");
+define("DB_PASS", "BirdWizard91");
+define("DB_NAME", "sabirds");
 
 // a2hosting
 // define("DB_SERVER", "localhost");
