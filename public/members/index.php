4531a8816d3ae6ba1c84b64af1304bad53b61525
ashleerollice@MB-P <ashleerollice@students.abtech.edu>
2024-12-04 12:47:02 -0500

    Asgn10 Complete

--- public/members/index.php
+++ public/members/index.php
@@ -9,63 +9,51 @@
   <div class="members_listing">
     <h1>Members/Users</h1>
     <?php if ($session->is_logged_in()) { ?>
-      <p>User: <?php echo h($session->username); ?> - Logged In</p>
-    <?php } else { ?>
-      <p>You are not logged in. <a href="<?php echo url_for('members/login.php'); ?>">Log in here</a>.</p>
+      <p>User: <?php echo h($session->username); ?></p>
     <?php } ?>
 
     <div class="actions">
-      <?php if ($session->is_logged_in()) { ?>
-        <a class="action" href="<?php echo url_for('members/new.php'); ?>">Add User</a>
-      <?php } else { ?>
-        <p><small>(Log in to add user)</small></p>
-      <?php } ?>
+      <a class="action" href="<?php echo url_for('members/new.php'); ?>">Add User</a>
     </div>
-
-    <table class="list" border="1">
-      <tr>
-        <th>ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Username</th>
-        <th>&nbsp;</th>
-        <th>&nbsp;</th>
-        <th>&nbsp;</th>
-      </tr>
-
-      <?php if (empty($members)) { ?>
-        <tr>
-          <td colspan="8">No users found.</td>
-        </tr>
-      <?php } else { ?>
-        <?php foreach ($members as $member) { ?>
-          <tr>
-            <td><?php echo h($member->id); ?></td>
-            <td><?php echo h($member->first_name); ?></td>
-            <td><?php echo h($member->last_name); ?></td>
-            <td><?php echo h($member->email); ?></td>
-            <td><?php echo h($member->username); ?></td>
-            <td><a href="<?php echo url_for('members/detail.php?id=' . h(u($member->id))); ?>">View</a></td>
-            <td>
-              <?php if ($session->is_logged_in()) { ?>
-                <a href="<?php echo url_for('members/edit.php?id=' . h(u($member->id))); ?>">Edit</a>
-              <?php } else { ?>
-                <span style="text-decoration: line-through; color: gray;">Edit</span>
-              <?php } ?>
-            </td>
-            <td>
-              <?php if ($session->is_logged_in()) { ?>
-                <a href="<?php echo url_for('members/delete.php?id=' . h(u($member->id))); ?>">Delete</a>
-              <?php } else { ?>
-                <span style="text-decoration: line-through; color: gray;">Delete</span>
-              <?php } ?>
-            </td>
-          </tr>
-        <?php } ?>
-      <?php } ?>
-    </table>
   </div>
+
+  <table class="list" border="1">
+    <tr>
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      <th>Username</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+    </tr>
+
+    <?php if (empty($members)) { ?>
+      <tr>
+        <td colspan="8">No users found.</td>
+      </tr>
+    <?php } else { ?>
+      <?php foreach ($members as $member) { ?>
+        <tr>
+          <td><?php echo h($member->id); ?></td>
+          <td><?php echo h($member->first_name); ?></td>
+          <td><?php echo h($member->last_name); ?></td>
+          <td><?php echo h($member->email); ?></td>
+          <td><?php echo h($member->username); ?></td>
+          <td><a href="<?php echo url_for('members/detail.php?id=' . h(u($member->id))); ?>">View</a></td>
+          <td>
+            <a href="<?php echo url_for('members/edit.php?id=' . h(u($member->id))); ?>">Edit</a>
+          </td>
+          <td>
+            <a href="<?php echo url_for('members/delete.php?id=' . h(u($member->id))); ?>">Delete</a>
+          </td>
+
+        </tr>
+      <?php } ?>
+    <?php } ?>
+  </table>
+</div>
 </div>
 
 <ul>
